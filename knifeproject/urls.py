891dd1db5f13ed0033b9a4ffac06b749e7a155f8
Zachary Thacker <zthacker@outlook.com>
2024-10-30 17:16:10 -0500

    wow I finally got it to work

--- knifeproject/urls.py
+++ knifeproject/urls.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from knifeapi.views import UserViewSet
-from knifeapi.views import KnifeView
-
+from knifeapi.views import UserViewSet, KnifeView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"knives", KnifeView, "knife")
+router.register(r"users", UserViewSet, "user")
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("login", UserViewSet.as_view({"post": "user_login"}), name="login"),
-    path(
-        "register", UserViewSet.as_view({"post": "register_account"}), name="register"
-    ),
+    path("api/", include(router.urls)),  # Adds 'api/' prefix to all routes
+    path("admin/", admin.site.urls),
 ]
